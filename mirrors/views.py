1b800f2c9114fa3bf98956f02d96bdd0b2b86f47
PyroPeter <abi1789@googlemail.com>
2010-10-13 19:02:00 +0200

    Allow filtering of mirrorlist by IP-version
    Signed-off-by: PyroPeter <abi1789@googlemail.com>

--- mirrors/views.py
+++ mirrors/views.py
@@ -17,6 +17,9 @@ class MirrorlistForm(forms.Form):
     country = forms.MultipleChoiceField(required=False)
     protocol = forms.MultipleChoiceField(required=False)
     use_mirror_status = forms.BooleanField(required=False)
+    ip_version = forms.ChoiceField(required=False, label="IP version",
+        choices=(('4','IPv4'), ('6','IPv6'), ('0','both')),
+        help_text="Which version of IP does your connection support?")
 
     def __init__(self, *args, **kwargs):
         super(MirrorlistForm, self).__init__(*args, **kwargs)
@@ -38,13 +41,16 @@ def generate_mirrorlist(request):
             countries = form.cleaned_data['country']
             protocols = form.cleaned_data['protocol']
             use_status = form.cleaned_data['use_mirror_status']
-            return find_mirrors(request, countries, protocols, use_status)
+            ipv4_supported = form.cleaned_data['ip_version'] in '40'
+            ipv6_supported = form.cleaned_data['ip_version'] in '60'
+            return find_mirrors(request, countries, protocols,
+                    use_status, ipv4_supported, ipv6_supported)
     else:
         form = MirrorlistForm()
 
     return direct_to_template(request, 'mirrors/index.html', {'mirrorlist_form': form})
 
-def find_mirrors(request, countries=None, protocols=None, use_status=False):
+def find_mirrors(request, countries=None, protocols=None, use_status=False, ipv4_supported=True, ipv6_supported=True):
     if not protocols:
         protocols = MirrorProtocol.objects.filter(
                 is_download=True).values_list('protocol', flat=True)
@@ -54,6 +60,13 @@ def find_mirrors(request, countries=None, protocols=None, use_status=False):
     )
     if countries and 'all' not in countries:
         qset = qset.filter(mirror__country__in=countries)
+    if ipv4_supported and not ipv6_supported:
+        qset = qset.filter(has_ipv4=True)
+    elif ipv6_supported and not ipv4_supported:
+        qset = qset.filter(has_ipv6=True)
+    elif not ipv4_supported and not ipv6_supported:
+        qset = qset.none()
+
     if not use_status:
         urls = qset.order_by('mirror__country', 'mirror__name', 'url')
         template = 'mirrors/mirrorlist.txt'
